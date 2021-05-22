e79dd545c99eb666329e1c8656f4532784b7f19a
= <liliasalas@lilias-imac.myfiosgateway.com>
2021-05-22 10:59:46 -0400

    adding some code

---
+++ script.js
@@ -0,0 +1,24 @@
+// The game starts when user clicks the let's play button
+// Assign a number to each image/card
+// Generate a random number to select the image to be displayed
+// When user clicks an image on the board display a check mark
+// When user clicks the loteria button, check if the numbers on the cards dealt match the divs,
+// if they match display win message and ask if user wants to keep playing
+// if they do not match display error message for 5 secs and continue playing
+//
+let cards = [],
+const div1 = 1,
+const div2 = 2,
+const div3 = 3,
+const div4 = 4,
+const div5 = 5,
+const div6 = 6,
+const div7 = 7,
+const div8 = 8,
+const div9 = 9,
+
+
+
+const game = function() {
+
+}
